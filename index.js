b1c748364e46e9a354f741ed767e6a3a6774852b
Iuliana6 <iulianacapilnar@yahoo.com>
2022-01-16 04:44:20 +0200

    Defined network paths

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require("express"); // Requires the Express module 
 const http = require('http'); // Calls the express function to start a new Express application 
 http.createServer(app).listen(3000); // start the server
+app.get('/lessons', function (req, res) {
+
+})
+app.post('/order', function (req, res) {
+
+})
+app.put('/lessons', function (req, res) {
+  
+})
+app.get('/search', function (req, res) {
+    
+})
 console.log('Server started!')
