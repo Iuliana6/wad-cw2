52bb92d5200492d9d82ce209e69418aeb3874302
Iuliana6 <iulianacapilnar@yahoo.com>
2022-01-16 04:44:46 +0200

    Connected to database

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express"); // Requires the Express module 
 const http = require('http'); // Calls the express function to start a new Express application 
 http.createServer(app).listen(3000); // start the server
+let db;
+const uri = "mongodb+srv://nodejs:nodejs@cluster0.chgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+MongoClient.connect(uri, (err, client) => {
+    db = client.db('cw2-db')
+})
 app.get('/lessons', function (req, res) {
 
 })
